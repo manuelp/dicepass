dca8b4b17c07233af2a82620d7a943c3a8c3e522
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-05-09 21:43:08 +0200

    First functions

--- src/dicepass/core.clj
+++ src/dicepass/core.clj
@@ -1,6 +1,11 @@
 (ns dicepass.core)
 
-(defn -main
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
+(defn throw-dice 
+  "Throws a `n`-faces dice."
+  [n]
+  (inc (rand-int n)))
+
+(defn throw-dices 
+  "Throws `num` dices with `faces`... faces."
+  [num faces]
+  (repeatedly num #(throw-dice faces)))
