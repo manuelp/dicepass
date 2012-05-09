de007f73b1ea30736bbf5b20f00b0f75e426d576
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-05-09 23:47:49 +0200

    Changed input dictionary format

--- src/dicepass/dictionary.clj
+++ src/dicepass/dictionary.clj
@@ -4,9 +4,9 @@
 (defn create-dictionary
   [filename]
   (letfn [(extract-entries []
-            (re-seq #"[0-9]{5}\t\w+" (slurp filename)))]
-    (loop [coll (map #(split % #"\t") (extract-entries))
-          dict {}]
-     (if (empty? coll)
-       dict
-       (recur (rest coll) (assoc dict (ffirst coll) (second (first coll))))))))
+            (re-seq #"[0-9]{5} \w+" (slurp filename)))]
+    (loop [coll (map #(split % #" ") (extract-entries))
+           dict {}]
+      (if (empty? coll)
+        dict
+        (recur (rest coll) (assoc dict (ffirst coll) (second (first coll))))))))
