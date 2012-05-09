c1fa72c36bb47b822413a9c0d94931f06d5ab6c5
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2012-05-09 21:42:54 +0200

    More details in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject dicepass "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Diceware-based passphrases generator."
+  :url "https://github.com/manuelp/dicepass"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
